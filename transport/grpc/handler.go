536082a96ca3f40d2b65b634372aeba60b76c992
aliakseizhyrauliou <aliaksei.zhurauliou@yandex.by>
2023-11-21 18:14:50 +0300

    null reference error

--- transport/grpc/handler.go
+++ transport/grpc/handler.go
@@ -2,94 +2,70 @@ package grpc
 
 import (
 	"context"
-	"fmt"
 	"github.com/aliakseizhyrauliou/gRPCApiGo/internal/db"
 	"github.com/aliakseizhyrauliou/gRPCApiGo/internal/rocket"
 	rkt "github.com/aliakseizhyrauliou/gRPCApiGo/protos/rocket/v1"
 	"google.golang.org/grpc"
-	"log"
 	"net"
 )
 
 type GRPCRocketService struct {
 	rkt.UnimplementedRocketServiceServer
-	Service rocket.Service
+	rocket.Service
 }
 
 func (serv *GRPCRocketService) GetRocket(ctx context.Context, req *rkt.GetRocketRequest) (*rkt.GetRocketResponse, error) {
-	r, err := serv.Service.GetRocketByID(ctx, req.GetId())
+
+	rocket, err := serv.GetRocketByID(ctx, req.GetId())
 	if err != nil {
-		return &rkt.GetRocketResponse{}, err
+		return nil, err
 	}
 
-	return &rkt.GetRocketResponse{
-		Rocket: &rkt.Rocket{
-			Id:   r.ID,
-			Name: r.Name,
-			Type: r.Type,
-		},
-	}, nil
+	// Преобразование вашего внутреннего типа Rocket в протобуф-тип
+	protoRocket := &rkt.Rocket{
+		Id:   rocket.ID,
+		Name: rocket.Name,
+		Type: rocket.Type,
+	}
+
+	// Создание ответа
+	response := &rkt.GetRocketResponse{
+		Rocket: protoRocket,
+	}
+
+	return response, nil
+
 }
 
-func (serv *GRPCRocketService) AddRocket(ctx context.Context, req *rkt.AddRocketRequest) (*rkt.AddRocketResponse, error) {
-	r := req.GetRocket()
-
-	if (rocket.Service{}) == serv.Service {
-		log.Println("Failed to initialize rocketService")
-	}
-
-	savingRocket, err := serv.Service.InsertRocket(ctx, rocket.Rocket{
-		ID:   r.Id,
-		Name: r.Name,
-		Type: r.Type,
-	})
-
-	if err != nil {
-		return &rkt.AddRocketResponse{}, err
-	}
-
-	// Assuming savingRocket is a pointer to Rocket, dereference it
-	return &rkt.AddRocketResponse{
-		Rocket: &rkt.Rocket{
-			Id:   savingRocket.ID,
-			Name: savingRocket.Name,
-			Type: savingRocket.Type,
-		},
-	}, nil
+func (srv *GRPCRocketService) AddRocket(context.Context, *rkt.AddRocketRequest) (*rkt.AddRocketResponse, error) {
+	return &rkt.AddRocketResponse{}, nil
 }
 
-func (serv *GRPCRocketService) DeleteRocket(ctx context.Context, req *rkt.DeleteRocketRequest) (*rkt.DeleteRocketResponse, error) {
-	r := req.GetRocket()
-
-	err := serv.Service.DeleteRocket(ctx, r.Id)
-
-	if err != nil {
-		return &rkt.DeleteRocketResponse{Status: "error"}, err
-	}
-
-	return &rkt.DeleteRocketResponse{Status: "ok"}, nil
+func (srv *GRPCRocketService) DeleteRocket(context.Context, *rkt.DeleteRocketRequest) (*rkt.DeleteRocketResponse, error) {
+	return &rkt.DeleteRocketResponse{}, nil
 }
 
 // StartGRPCServer starts Ports GRPC server
-func StartGRPCServer(db db.Store) error {
-	grpcServer := grpc.NewServer()
-	rocketService := rocket.New(db)
-
-	if (rocket.Service{}) == rocketService {
-		return fmt.Errorf("Failed to initialize rocketService")
-	}
-
-	grpcService := GRPCRocketService{
-		Service: rocketService,
-	}
-
-	rkt.RegisterRocketServiceServer(grpcServer, &grpcService)
-
-	con, err := net.Listen("tcp", ":50051")
+func StartGRPCServer(store db.Store) error {
+	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		panic(err)
+		return err
 	}
-	err = grpcServer.Serve(con)
+
+	grpcServer := grpc.NewServer()
+
+	rktService := NewGRPCRocketService(store)
+	rkt.RegisterRocketServiceServer(grpcServer, rktService)
+
+	if err := grpcServer.Serve(listener); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func NewGRPCRocketService(store db.Store) *GRPCRocketService {
+
+	service := rocket.New(store)
+	return &GRPCRocketService{Service: service}
 }
